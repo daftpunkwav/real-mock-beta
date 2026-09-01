fcd1dd94db21cf0854e5d2e4f63c518e37fa0534
daftpunkwav <daftpunk.wav@outlook.com>
2026-09-01 20:15:12 +0800

    chore(web): 删除未引用的 effects 动画组件包

--- apps/web/src/components/effects/index.ts
+++
@@ -1,6 +0,0 @@
-export { ParticleField } from "./ParticleField";
-export { FluidBackground } from "./FluidBackground";
-export { AnimatedCounter } from "./AnimatedCounter";
-export { FadeInView } from "./FadeInView";
-export { StaggerContainer, StaggerItem } from "./StaggerContainer";
-export { MagneticButton } from "./MagneticButton";
